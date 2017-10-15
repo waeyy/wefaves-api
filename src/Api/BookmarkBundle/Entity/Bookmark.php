541696e6cb9af03d843a0d60af21481b50ac50c6
Djamil <soidikid@gmail.com>
2017-10-15 13:01:19 +0200

    fix empty title

--- src/Api/BookmarkBundle/Entity/Bookmark.php
+++ src/Api/BookmarkBundle/Entity/Bookmark.php
@@ -82,13 +82,11 @@ class Bookmark
     protected $parentId;
 
     /**
-     * @ORM\Column(name="title", type="string", length=255)
+     * @ORM\Column(name="title", type="string", length=255, nullable=true)
      *
      * @Serializer\Groups({"GET_BOOKMARKS_FOLDERS"})
      * @Serializer\Expose
      *
-     * @Assert\NotBlank()
-     * @Assert\NotNull()
      */
     protected $title;
 
